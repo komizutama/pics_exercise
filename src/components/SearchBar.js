b148067453223ca99ab04607a0efbf41c183937d
komizutama <komizutama@gmail.com>
2021-03-29 12:49:33 -0400

    uncontrolled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
+
+
+
 class SearchBar extends React.Component {
+
+    onInputChange(event) {
+        console.log(event.target.value);//this contains the text user inPut
+    }
+
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" onChange={this.onInputChange} />
+                        {/* note no parenthesis for callback function passed to event handler like onChange  currently uncontrolled.*/}
                     </div>
                 </form>
             </div>
