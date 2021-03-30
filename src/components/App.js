1532da0f36d81b7ea57d347e03e67a0dba617255
komizutama <komizutama@gmail.com>
2021-03-29 21:15:31 -0400

    adding ability to hit enter and collect data

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 
+
 const App = () => {
     return (
         <div className="ui container" style={{marginTop: '10px'}}>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,20 +4,27 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+    state = { term: '' };
 
-    onInputChange(event) {
-        console.log(event.target.value);//this contains the text user inPut
+    onFormSubmit = (event) => {
+        event.preventDefault();//this line of code prevents the form from submiting automatically
+        // using an arroe function stops the dreaded undefined.. but can also be used with an arrow function in the callback 
+        console.log (this.state.term);
     }
 
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
-                        {/* note no parenthesis for callback function passed to event handler like onChange  currently uncontrolled.*/}
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={ e => this.setState({ term: e.target.value })}
+                        />
+                        {/* note no parenthesis for callback function passed to event handler like onChange  now controlled via state.*/}
                     </div>
                 </form>
             </div>
