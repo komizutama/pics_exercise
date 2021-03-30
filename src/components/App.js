26b61107a431f57bbc0caf09b22ef60173458e17
komizutama <komizutama@gmail.com>
2021-03-30 17:34:51 -0400

    refactored to async

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,18 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        axios
+    async onSearchSubmit(term){
+        const response = await axios
         .get('https://api.unsplash.com/search/photos', {
             params: {query: term},
             headers: {
                 Authorization: 
                     'Client-ID 7yYrLjx33EXr_h9Xn2r1J0d0SfsD4tARUzFdcLTN9VQ'
-            },
-        })
-        .then(response => {
-            console.log(response.data.results);
-        }); //axios returns a promise so you tag on a then statement to do whatever they request.
-        // console.log(term);
+            }
+        });
+        console.log(response.data.results);
+//async await syntex async goes in front of method name take whatever is getting resolved.. here axios.get.. then assign to a variable.
+
     }
 
     render () {
